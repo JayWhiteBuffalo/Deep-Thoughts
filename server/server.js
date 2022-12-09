d3e492856e88a7e126c27fddb4503d314bc413b4
White Buffalo <apothocare@gmail.com>
2022-12-09 15:08:02 -0600

    Server and Client set and ready for production env testing

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-
+const path = require('path');
 const express = require('express');
 // import ApollowServer
 const { ApolloServer } = require('apollo-server-express');
@@ -29,6 +29,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
